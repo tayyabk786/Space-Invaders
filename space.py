1244e4f30c0cd59a521d8f206af2af82ae6616e3
tayyabk786 <22310780@bucks.ac.uk>
2023-10-26 13:07:49 +0100

    Update space.py
    Co-Authored-By: Hamza1025 <39687436+hamza1025@users.noreply.github.com>

--- space.py
+++ space.py
@@ -48,7 +48,7 @@ while run:
             run = False
     
     pygame.display.update()
-
+    # defining button, initialising its properties
 class Button():
     def _init_(self, image, pos, text_input, font, base_color, hovering_color):
         self.image = image
@@ -62,7 +62,21 @@ class Button():
             self.image = self.text
         self.rect = self.image.get_rect(center=(self.y_pos))
         self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
-
-def upda
+#update screen puts its image and text on screen
+def update(self, screen):
+    if self.image is not None:
+        screen.blit(self.image, self.rect)
+    screen.blit(self.text, self.text_rect)
+#check for input checks if we are clicking on the button
+def checkforInput(self, position):
+    if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+        return True
+    return False
+#change color is for if mouse is hovering over button it will change to desired colour
+def changeColor(self, position):
+    if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+        self.text = self.font.render(self.text_input, True, self.hovering_color)
+    else:
+        self.text = self.font.render(self.text_input, True, self.base_color)
 
 pygame.quit
