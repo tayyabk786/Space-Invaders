ef13e0c5485335df110bbed80ff1aa930822bc0c
Hamza1025 <39687436+Hamza1025@users.noreply.github.com>
2023-11-01 23:40:46 +0000

    Update invader.py

--- MainFolder/invader.py
+++ MainFolder/invader.py
@@ -109,6 +109,49 @@ def start_game():
     def game_over_text():
         over_text = over_font.render("GAME OVER", True, (125, 225, 225))
         screen.blit(over_text, (200, 250))
+        # Buttons
+        class Button:
+            def __init__(self, y, x, width, height, text, action, text_colour=BUTTON_TEXT_COLOUR):
+                self.rect = pygame.Rect(y, x, width, height)
+                self.text = text
+                self.action = action
+                self.clicked = False
+                self.text_colour = text_colour  
+        
+            def draw(self):
+                pygame.draw.rect(screen, BUTTON_COLOUR, self.rect)
+                text_surface = BUTTON_FONT.render(self.text, True, self.text_colour)  
+                text_rect = text_surface.get_rect(center=self.rect.center)
+                screen.blit(text_surface, text_rect)
+        
+            # Create buttons
+            button1 = Button(300, 250, 200, 50, "RESTART GAME", "restart_game")
+            button2 = Button(300, 400, 200, 50, "Quit", "quit")
+            button3 = Button(250, 80, 285, 100, "GAME OVER", "quit", GAME_OVER_COLOUR)  # Game over colour red
+            
+            buttons = [button1, button2, button3]
+            
+            # Main game loop
+            running = True
+            
+            while running:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                        pos = pygame.mouse.get_pos()
+                        for button in buttons:
+                            if button.rect.collidepoint(pos):
+                                if button.action == "restart_game":
+                                    # Add your start game logic here
+                                    print("RESTART GAME!")
+                                elif button.action == "quit":
+                                    running = False
+            
+                # Draw buttons
+                for button in buttons:
+                    button.draw()
+        
 
     # Initialize the game
     playerImg = pygame.image.load("MainFolder/images/spaceship.png")
